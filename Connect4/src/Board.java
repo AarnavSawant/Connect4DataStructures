6786117cdb64d964a27ecb317ebc420b04c3ed32
phenom4n4n <61065078+phenom4n4n@users.noreply.github.com>
2022-10-06 10:19:53 -0700

    board drop logic

--- Connect4/src/Board.java
+++ Connect4/src/Board.java
@@ -1,3 +1,40 @@
+import java.util.Arrays;
+
 public class Board {
-    
+    public static final int WIDTH = 7;
+    public static final int HEIGHT = 6;
+
+    private int[][] board;
+
+    public Board() {
+        board = new int[HEIGHT][WIDTH];
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : board) {
+            for (int col : row) {
+                sb.append(col).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public int[][] getBoard() {
+        int[][] copy = new int[HEIGHT][WIDTH];
+        for (int i = 0; i < HEIGHT; i++) {
+            copy[i] = Arrays.copyOf(board[i], WIDTH);
+        }
+        return copy;
+    }
+
+    public void addPiece(int col, int player) {
+        for (int i = HEIGHT - 1; i > -1; i++) {
+            if (board[i][col] == 0) {
+                board[i][col] = player;
+                return;
+            }
+        }
+    }
 }
--- Connect4/src/ConnectFourTester_5SinghSawant.java
+++ Connect4/src/ConnectFourTester_5SinghSawant.java
@@ -1,2 +1,8 @@
 public class ConnectFourTester_5SinghSawant {
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println(board);
+        board.addPiece(3, 1);
+        System.out.println(board);
+    }
 }
